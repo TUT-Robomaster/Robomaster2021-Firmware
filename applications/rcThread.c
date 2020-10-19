9f86c8508541298e5a3cccb14822983d7bab60c2
Jasonlecson <lujinmiao@outlook.com>
2020-10-19 23:26:01 +0800

    更改了设备初始化架构

--- applications/rcThread.c
+++ applications/rcThread.c
@@ -1,9 +1,5 @@
-#include <rtthread.h>
-#include <rtdevice.h>
 #include "rcVariables.h"
 
-
-#define DBUS "uart1"
 
 //接收数据回调函数
 static rt_err_t dbus_input(rt_device_t dev,rt_size_t size)
@@ -34,18 +30,17 @@ static void dbus_thread_entry(void *parameter)
         result = rt_mq_recv(&dbus_rx_mq, &dbus_msg, sizeof(dbus_msg), RT_WAITING_FOREVER);
         if (result == RT_EOK)
         {
-					rt_device_read(dbus_msg.dev, 0, rx_buffer, dbus_msg.size);
-					RC_Process(&rc,rx_buffer);
+					rt_device_read(dbus_msg.dev, 0, dbus_rx_buffer, dbus_msg.size);
+					RC_Process(&rc,dbus_rx_buffer);
         }
     }
 }
 
-rt_err_t dbus_dma_init(void)
+rt_err_t dbus_open(void)
 {
-	rt_err_t ret = RT_EOK;
+	rt_err_t result;
 	static char msg_pool[256];
-	//查找串口设备
-	dbus = rt_device_find(DBUS);
+
 	if(!dbus)
 	{
 		rt_kprintf("find %s failed! \n",DBUS);
@@ -57,55 +52,39 @@ rt_err_t dbus_dma_init(void)
                sizeof(struct rx_msg),    /* 一条消息的最大长度 */
                sizeof(msg_pool),         /* 存放消息的缓冲区大小 */
                RT_IPC_FLAG_FIFO);        /* 如果有多个线程等待，按照先来先得到的方法分配消息 */
-	dbus_control();//配置串口参数
-	/* 以 DMA 接收及轮询发送方式打开串口设备 */
-	rt_device_open(dbus, RT_DEVICE_FLAG_DMA_RX);
+
   /* 设置接收回调函数 */
   rt_device_set_rx_indicate(dbus, dbus_input);
 	//创建dbus接受函数
-	rt_thread_t dbus_receive_thread = rt_thread_create("dbus_receive",dbus_thread_entry,RT_NULL,256,10,1);
+	result  = rt_thread_init(&dbus_receive_thread,
+							 "dbus_receive",
+							 dbus_thread_entry,
+							 RT_NULL,
+							 dbus_thread_stack,
+							 STACK_SIZE,
+							 THREAD_PRIORITY,
+							 TIMESLICE);
 	//创建成功则启动线程
-	if(dbus_receive_thread != RT_NULL)
+	if(result == RT_EOK)
 	{
-		rt_thread_startup(dbus_receive_thread);
+		rt_thread_startup(&dbus_receive_thread);
 	}
-	else
-	{
-		ret = RT_ERROR;
-	}
-	return ret;
+	return result;
 }
 
-rt_err_t dbus_control(void)
+void RC_Process(rc_info_t *rc,uint8_t *dbus_rx_buffer)
 {
-	struct serial_configure dbus_config;
-	
-	dbus_config.baud_rate = 100000;
-	dbus_config.data_bits = DATA_BITS_8;
-	dbus_config.stop_bits = STOP_BITS_1;
-	dbus_config.parity = PARITY_NONE;
-	dbus_config.bit_order = BIT_ORDER_LSB;
-	dbus_config.invert = NRZ_NORMAL;
-	dbus_config.bufsz = 19; //设置为18无法运行，暂时不知道为什么
-	dbus_config.reserved = 0;
-	
-	rt_device_control(dbus,RT_DEVICE_CTRL_CONFIG,&dbus_config);
-	return RT_EOK;
-}
-
-void RC_Process(rc_info_t *rc,uint8_t *rx_buffer)
-{
-	  rc->ch1 = (rx_buffer[0] | rx_buffer[1] << 8) & 0x07FF;
+	  rc->ch1 = (dbus_rx_buffer[0] | dbus_rx_buffer[1] << 8) & 0x07FF;
   rc->ch1 -= 1024;
-  rc->ch2 = (rx_buffer[1] >> 3 | rx_buffer[2] << 5) & 0x07FF;
+  rc->ch2 = (dbus_rx_buffer[1] >> 3 | dbus_rx_buffer[2] << 5) & 0x07FF;
   rc->ch2 -= 1024;
-  rc->ch3 = (rx_buffer[2] >> 6 | rx_buffer[3] << 2 | rx_buffer[4] << 10) & 0x07FF;
+  rc->ch3 = (dbus_rx_buffer[2] >> 6 | dbus_rx_buffer[3] << 2 | dbus_rx_buffer[4] << 10) & 0x07FF;
   rc->ch3 -= 1024;
-  rc->ch4 = (rx_buffer[4] >> 1 | rx_buffer[5] << 7) & 0x07FF;
+  rc->ch4 = (dbus_rx_buffer[4] >> 1 | dbus_rx_buffer[5] << 7) & 0x07FF;
   rc->ch4 -= 1024;
 	
-  rc->sw1 = ((rx_buffer[5] >> 4) & 0x000C) >> 2;
-  rc->sw2 = (rx_buffer[5] >> 4) & 0x0003;
+  rc->sw1 = ((dbus_rx_buffer[5] >> 4) & 0x000C) >> 2;
+  rc->sw2 = (dbus_rx_buffer[5] >> 4) & 0x0003;
   
   if ((abs(rc->ch1) > 660) || \
       (abs(rc->ch2) > 660) || \
@@ -115,15 +94,15 @@ void RC_Process(rc_info_t *rc,uint8_t *rx_buffer)
     rt_memset(rc, 0, sizeof(rc_info_t));
   }		
 	
-	rc->mouse.x = ((int16_t)rx_buffer[6]) | ((int16_t)rx_buffer[7] << 8);     
-	rc->mouse.y = ((int16_t)rx_buffer[8]) | ((int16_t)rx_buffer[9] << 8);     
-	rc->mouse.z = ((int16_t)rx_buffer[10]) | ((int16_t)rx_buffer[11] << 8);    
+	rc->mouse.x = ((int16_t)dbus_rx_buffer[6]) | ((int16_t)dbus_rx_buffer[7] << 8);     
+	rc->mouse.y = ((int16_t)dbus_rx_buffer[8]) | ((int16_t)dbus_rx_buffer[9] << 8);     
+	rc->mouse.z = ((int16_t)dbus_rx_buffer[10]) | ((int16_t)dbus_rx_buffer[11] << 8);    
  
-  rc->mouse.press_l = rx_buffer[12];
-  rc->mouse.press_r = rx_buffer[13];
+  rc->mouse.press_l = dbus_rx_buffer[12];
+  rc->mouse.press_r = dbus_rx_buffer[13];
 	
-  rc->key = ((int16_t)rx_buffer[14]) | ((int16_t)rx_buffer[15] << 8);
+  rc->key = ((int16_t)dbus_rx_buffer[14]) | ((int16_t)dbus_rx_buffer[15] << 8);
 	
-	rc->sw  = (rx_buffer[16] | rx_buffer[17] << 8) & 0x07FF;
+	rc->sw  = (dbus_rx_buffer[16] | dbus_rx_buffer[17] << 8) & 0x07FF;
 	rc->sw -= 1024;
 }
--- applications/rcVariables.h
+++ applications/rcVariables.h
@@ -1,5 +1,12 @@
 #include <rtthread.h>
+#include <rtdevice.h>
+#define THREAD_PRIORITY 10
+#define STACK_SIZE 256
+#define TIMESLICE 1
 
+#define DBUS "uart1"
+
+//结构体定义
 typedef struct
 {
 	/* mouse move information */
@@ -36,12 +43,16 @@ struct rx_msg
 	rt_device_t dev;
 	rt_size_t size;
 };
-
-void RC_Process(rc_info_t *rc,uint8_t *rx_buffer);
+//函数相关
+void RC_Process(rc_info_t *rc,uint8_t *dbus_rx_buffer);
 rt_err_t dbus_control(void);
 
-uint8_t rx_buffer[18];//串口接收缓冲区
+
 rc_info_t rc; //遥控器通道结构体变量
 
-static rt_device_t dbus; //串口设备句柄
+extern rt_device_t dbus; //串口设备句柄
 static struct rt_messagequeue dbus_rx_mq; //消息队列控制块
+static uint8_t dbus_rx_buffer[18];//串口接收缓冲区
+
+static rt_uint8_t dbus_thread_stack[STACK_SIZE];//线程栈
+static struct rt_thread dbus_receive_thread;
